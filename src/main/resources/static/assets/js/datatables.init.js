f1579ad1273ed1e88f9e67bc37d8230605b59b9e
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2022-10-16 22:05:36 +0700

    up file

---
+++ src/main/resources/static/assets/js/datatables.init.js
@@ -0,0 +1,18 @@
+// let dataTable = null
+// $(document).ready(function () {
+//     dataTable = $("#datatable").DataTable({
+//         columns: [
+//             { data: 'expenses' },
+//             { data: 'price' },
+//             { data: 'use' },
+//             { data: 'createBy' },
+//             { data: 'createdDate' },
+//             { data: 'modifyBy' },
+//             { data: 'modifiedDate' },
+//             { data: 'note' },
+//             { data: 'action' },
+//         ]
+//     }), $("#datatable-buttons").DataTable({
+//         buttons: ["copy", "excel", "pdf", "colvis"],
+//     }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $(".dataTables_length select").addClass("form-select form-select-sm")
+// });
